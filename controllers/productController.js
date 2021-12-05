b9dfe8cf897cb1fa82a945e79d1290f43598da8b
Megharth <m.c.lakhataria@gmail.com>
2021-12-05 09:02:01 -0500

    add route for adding reviews

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,60 +1,77 @@
-import productModel from '../models/productModel.js'
+import productModel from "../models/productModel.js";
 
-const createProduct = async(req, res, _next) => {
-    const { product } = req.body;
-    const newProduct = await productModel.create(product);
-    res.json({ newProduct });
-}
+const createProduct = async (req, res, _next) => {
+  const { product } = req.body;
+  const newProduct = await productModel.create(product);
+  res.json({ newProduct });
+};
 
+const addReview = async (req, res, _next) => {
+  const { review } = req.body;
+  const { id } = req.params;
+  const product = await productModel.findById(id);
+  if (product) {
+    const reviewObj = {
+      content: review,
+      created: new Date(),
+      updated: new Date(),
+    };
+    product.reviews.push(reviewObj);
+    await productModel.update(id, product);
+    const updatedProduct = await productModel.findById(id);
+    res.json({ updatedProduct });
+  } else {
+    res.json({ success: false, error: "Product not found" });
+  }
+};
 const findAllProducts = async (_req, res, _next) => {
-    const products = await productModel.findAll();
-    if(products.length > 0) {
-        res.json({ products });
-    } else {
-        res.json({ success: false, error: "Products not found"});
-    }
-}
+  const products = await productModel.findAll();
+  if (products.length > 0) {
+    res.json({ products });
+  } else {
+    res.json({ success: false, error: "Products not found" });
+  }
+};
 
 const findProduct = async (req, res, _next) => {
-    const { id } = req.params;
-    const product = await productModel.findById(id);
-    if(product) {
-        res.json({ product });
-    } else {
-        res.json({ success: false, error: "Product not found"});
-    }
-}
+  const { id } = req.params;
+  const product = await productModel.findById(id);
+  if (product) {
+    res.json({ product });
+  } else {
+    res.json({ success: false, error: "Product not found" });
+  }
+};
 
-const updateProduct = async(req, res, _next) => {
-    const { id } = req.params;
-    const product = await productModel.findById(id);
-    if(product) {
-        const { product } = req.body;
-        await productModel.update(id, product);
-        const updatedProduct = await productModel.findById(id);
-        res.json({ updatedProduct });
-    } else {
-        res.json({ success: false, error: "Product not found"});
-    }
-    
-}
+const updateProduct = async (req, res, _next) => {
+  const { id } = req.params;
+  const product = await productModel.findById(id);
+  if (product) {
+    const { product } = req.body;
+    await productModel.update(id, product);
+    const updatedProduct = await productModel.findById(id);
+    res.json({ updatedProduct });
+  } else {
+    res.json({ success: false, error: "Product not found" });
+  }
+};
 
-const deleteProduct = async(req, res, _next) => {
-    const { id } = req.params;
-    const product = await productModel.findById(id);
-    if(product) {
-        await productModel.deleteOne(id);
-        res.json({ success: true });
-    } else {
-        res.json({ success: false, error: "Product not found"});
-    }
-}
-
+const deleteProduct = async (req, res, _next) => {
+  const { id } = req.params;
+  const product = await productModel.findById(id);
+  if (product) {
+    await productModel.deleteOne(id);
+    res.json({ success: true });
+  } else {
+    res.json({ success: false, error: "Product not found" });
+  }
+};
 
 export default {
-    createProduct,
-    findAllProducts,
-    findProduct,
-    updateProduct,
-    deleteProduct,
-}
+  createProduct,
+  addReview,
+  findAllProducts,
+  findProduct,
+  updateProduct,
+  deleteProduct,
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import productController from '../controllers/productController.js';
+import express from "express";
+import productController from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.post('/', productController.createProduct);
-router.get('/', productController.findAllProducts);
-router.get('/:id', productController.findProduct);
-router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.post("/", productController.createProduct);
+router.post("/:id/add-review", productController.addReview);
+router.get("/", productController.findAllProducts);
+router.get("/:id", productController.findProduct);
+router.put("/:id", productController.updateProduct);
+router.delete("/:id", productController.deleteProduct);
 
 export default router;
