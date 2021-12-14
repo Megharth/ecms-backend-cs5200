0519d19983cc415ce047df545f3369f7dcf462c3
Megharth <m.c.lakhataria@gmail.com>
2021-12-14 01:08:06 -0500

    add routes for orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import orderController from '../controllers/orderController.js';
+import express from "express";
+import orderController from "../controllers/orderController.js";
 
 const router = express.Router();
 
-router.post('/', orderController.createOrder);
-router.get('/', orderController.findAllOrders);
-router.get('/:id', orderController.findOrder);
-router.put('/:id', orderController.updateOrder);
-router.delete('/:id', orderController.deleteOrder);
+router.post("/", orderController.createOrder);
+router.post("/bulk", orderController.createBulkOrder);
+router.get("/", orderController.findAllOrders);
+router.get("/:id", orderController.findOrder);
+router.put("/:id", orderController.updateOrder);
+router.delete("/:id", orderController.deleteOrder);
 
 export default router;
